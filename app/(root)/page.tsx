554ac469ea855892aa2b0c500f884cb864d737b7
ycabreja <ycabreja279@gmail.com>
2024-10-29 16:49:37 -0400

    Right Side Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Yordelie' };
+    const loggedIn = { firstName: 'Yordelie', lastName: 'Cabreja', email: 'yordelie@email.com' };
 
     return (
         <section className='home'>
@@ -21,7 +22,14 @@ const Home = () => {
                         totalCurrentBalance={1250.35}
                     />
                 </header>
+                RECENT TRANSACTION
             </div>
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 125.80 }, { currentBalance: 600.80 }]}
+
+            />
         </section>
     )
 }
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -2,26 +2,25 @@
 import React from 'react';
 import {
     Sheet,
+    SheetClose,
     SheetContent,
-    SheetDescription,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet";
-import Image from "next/image"
+import Image from "next/image";
 import Link from 'next/link';
 import { sidebarLinks } from '@/constants';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 
 const MobileNav = ({ user }: MobileNavProps) => {
     const pathname = usePathname();
     return (
-
         <section className='w-full max-w-[246px]'>
             <Sheet>
                 <SheetTrigger>
-
                     <Image
                         src="/icons/hamburger.svg"
                         width={30}
@@ -29,25 +28,63 @@ const MobileNav = ({ user }: MobileNavProps) => {
                         alt="menu"
                         className="cursor-pointer"
                     />
-
-
                 </SheetTrigger>
-                <SheetContent side="left">
+                <SheetContent side="left" className='border-none bg-white'>
+                    <SheetHeader>
+                        <SheetTitle>
+                            <VisuallyHidden>Navigation Menu</VisuallyHidden> {/* Accessible title */}
+                        </SheetTitle>
+                    </SheetHeader>
                     <nav className='flex flex-col gap-4'>
-                        <Link href="/" className='mb-12 cursor-pointer flex items-center gap-2'>
+                        <Link href="/" className='cursor-pointer flex items-center gap-1 px-4'>
                             <Image
                                 src="/icons/logo.svg"
                                 width={34}
                                 height={34}
                                 alt="Horizon Logo"
-                                className='size-[24px] max-xl:size-14'
                             />
-                            <h1 className='sidebar-logo'>Horizon</h1>
+                            <h1 className='text-[26px] font-ibm-plex-serif !text-bold text-black-1'>Horizon</h1>
                         </Link>
+                        <div className="mobilenav-sheet">
+                            <SheetClose asChild>
+                                <nav className='flex h-full flex-col gap-6 pt-16 text-white'>
+                                    {sidebarLinks.map((item) => {
+                                        const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+                                        return (
+                                            <SheetClose asChild key={item.route}>
+                                                <Link href={item.route} key={item.label}
+                                                    className={cn('mobilenav-sheet_close w-full ', { 'bg-bankGradient': isActive })}
+                                                >
+                                                    <Image
+                                                        src={item.imgURL}
+                                                        alt={item.label}
+                                                        width={20}
+                                                        height={20}
+
+                                                        className={cn({
+                                                            'brightness-[3] invert-0': isActive
+                                                        })}
+                                                    />
+
+
+                                                    <p className={cn('text-16 font-semibold text-black-2', {
+                                                        'text-white': isActive
+                                                    })}>
+                                                        {item.label}
+                                                    </p>
+                                                </Link>
+                                            </SheetClose>
+                                        );
+                                    })}
+                                    USER
+                                </nav>
+                            </SheetClose>
+                            FOOTER
+                        </div>
 
                         {sidebarLinks.map((item) => {
-
-                            const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+                            const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
                             return (
                                 <Link href={item.route} key={item.label}
                                     className={cn('sidebar-link', { 'bg-bankGradient': isActive })}
@@ -61,8 +98,6 @@ const MobileNav = ({ user }: MobileNavProps) => {
                                                 'brightness-[3] invert-0': isActive
                                             })}
                                         />
-
-
                                     </div>
 
                                     <p className={cn('sidebar-label', {
@@ -71,8 +106,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                                         {item.label}
                                     </p>
                                 </Link>
-                            )
-
+                            );
                         })}
                     </nav>
                 </SheetContent>
@@ -82,5 +116,6 @@ const MobileNav = ({ user }: MobileNavProps) => {
 };
 
 export default MobileNav;
+
 
 

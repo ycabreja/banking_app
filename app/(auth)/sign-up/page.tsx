284fbd90b48ceb4c4e397669bab8022f5d3e91cc
ycabreja <ycabreja279@gmail.com>
2024-11-04 12:45:10 -0500

    Sign in page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
+        <section className='flex-center size-full max-sm:px-6'>
+            <AuthForm type="sign-up" />
+
+        </section>
     )
 }
 

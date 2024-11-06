d45ac7103b81f718c8f0d07139d46ad3955a951d
ycabreja <ycabreja279@gmail.com>
2024-11-06 15:23:13 -0500

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import Footer from './Footer';
 
 const Sidebar = ({ user }: SiderbarProps) => {
 
@@ -58,7 +59,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
 
             </nav>
 
-            FOOTER
+            <Footer user={user} />
         </section>
     )
 }

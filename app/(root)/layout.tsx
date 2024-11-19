0cbaaa50fda07f53dfedab5e379547e83871089f
Roliver Javier Rodriguez <roliverjavier@gmail.com>
2024-11-19 11:49:52 -0400

    fixing issues

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,32 +1,30 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
-export default function RootLayout({
-    children,
+export default async function RootLayout({
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    const loggedIn = { firstName: 'Yordelie', lastName: 'Cabreja' };
+  const loggedIn = await getLoggedInUser(); //{ firstName: 'Yordelie', lastName: 'Cabreja' };
 
-    if (!loggedIn) redirect('/sign-in')
+  if (!loggedIn) redirect("/sign-in");
 
-    return (
-        <main className="flex h-screen w-full font-inter">
-            <Sidebar user={loggedIn} />
+  console.log("loggedIn=>", loggedIn);
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
 
-            <div className="flex size-full flex-col">
-                <div className="root-layout">
-                    <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
-                    <MobileNav user=
-                        {loggedIn} />
-                </div>
-                {children}
-            </div>
-        </main>
-
-    );
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <MobileNav user={loggedIn} />
+        </div>
+        {children}
+      </div>
+    </main>
+  );
 }
-
-
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,38 +1,37 @@
-import HeaderBox from '@/components/HeaderBox'
-import RightSidebar from '@/components/RightSidebar';
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import { getLoggedInUser } from '@/lib/actions/user.actions';
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = () => {
-    const loggedIn = { firstName: 'Yordelie', lastName: 'Cabreja', email: 'yordelie@test.com' };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser(); //{ firstName: 'Yordelie', lastName: 'Cabreja', email: 'yordelie@test.com' };
 
-    return (
-        <section className='home'>
-            <div className='home-content'>
-                <header className='home-header'>
-                    <HeaderBox
-                        type="greeting"
-                        title="Welcome"
-                        user={loggedIn?.firstName || 'Guest'}
-                        subtext="Access and manage your account and transactions efficiently."
-                    />
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-                    <TotalBalanceBox
-                        accounts={[]}
-                        totalBanks={1}
-                        totalCurrentBalance={1250.35}
-                    />
-                </header>
-                RECENT TRANSACTION
-            </div>
-            <RightSidebar
-                user={loggedIn}
-                transactions={[]}
-                banks={[{ currentBalance: 125.80 }, { currentBalance: 600.80 }]}
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 125.8 }, { currentBalance: 600.8 }]}
+      />
+    </section>
+  );
+};
 
-            />
-        </section>
-    )
-}
-
-export default Home
+export default Home;
